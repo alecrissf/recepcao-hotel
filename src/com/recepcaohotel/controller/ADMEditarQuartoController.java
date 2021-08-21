f52f40b22810f0bf72b25019882ad93b2d86d9d7
Cleomon Junior <cleomonjunior@gmail.com>
2021-08-21 16:54:21 -0300

    Implementação da classe ADMEditarQuartoController

--- src/com/recepcaohotel/controller/ADMEditarQuartoController.java
+++ src/com/recepcaohotel/controller/ADMEditarQuartoController.java
@@ -2,6 +2,11 @@ package com.recepcaohotel.controller;
 
 import java.io.IOException;
 
+import com.recepcaohotel.app.App;
+import com.recepcaohotel.controller.context.AdminContext;
+import com.recepcaohotel.model.Quarto;
+import com.recepcaohotel.model.Sistema;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -36,16 +41,51 @@ public class ADMEditarQuartoController {
     private Button botaoCancelar;
 
     @FXML
-    private void inicializar() {
-        // TODO: colocar os valores atuais nos devidos labels.
+    private void initialize() {
+        Sistema s = App.getSystemInstance();
+        AdminContext ctx = AdminContext.getInstance();
+        Quarto q = s.getQuarto(ctx.getQuartoSelecionado().getNumero());
+
+        textoQtdCamasDeCasalAtual.setText(String.valueOf(q.getQntdCamasCasal()));
+        textoQtdCamasDeSolteiroAtual.setText(String.valueOf(q.getQntdCamasSolteiro()));
+        textoValorDiariaAtual.setText(String.valueOf(q.getDiaria()));
     }
 
     @FXML
     private void confirmar(ActionEvent event) {
-        // TODO: Mudar as informações usando a instância da classe sistema.
+        Sistema s = App.getSystemInstance();
+        AdminContext ctx = AdminContext.getInstance();
+        Quarto q = s.getQuarto(ctx.getQuartoSelecionado().getNumero());
 
         // OBS.: campos vazios significam não mudança.
+        try {
+            if (campoQtdCamasDeCasal.getText() != "") {
+                q.setQntdCamasCasal(Integer.parseInt(campoQtdCamasDeCasal.getText()));
+            }
+        } catch (NumberFormatException e) {
+            q.setQntdCamasCasal(Integer.parseInt("0"));
+            e.printStackTrace();
+        }
 
+        try {
+            if (campoQtdCamasDeSolteiro.getText() != "") {
+                q.setQntdCamasSolteiro(Integer.parseInt(campoQtdCamasDeSolteiro.getText()));
+            }
+        } catch (NumberFormatException e) {
+            q.setQntdCamasSolteiro(Integer.parseInt("0"));
+            e.printStackTrace();
+        }
+
+        try {
+            if (campoValorDiaria.getText() != "") {
+                q.setDiaria(Float.parseFloat(campoValorDiaria.getText()));
+                ;
+            }
+        } catch (NumberFormatException e) {
+            q.setDiaria(Float.parseFloat("0.0"));
+            e.printStackTrace();
+        }
+        s.adicionarQuarto(q);
         if (event.getSource() == botaoConfirmar) {
             voltar(botaoConfirmar);
         }
