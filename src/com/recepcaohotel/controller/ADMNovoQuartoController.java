338b0e2bf082a9f0599e5f3ef99b0c47906ed65a
Josué Araújo <jjosuearaujo170@gmail.com>
2021-08-22 12:23:33 -0300

    ADMNovoQuartoController

--- src/com/recepcaohotel/controller/ADMNovoQuartoController.java
+++ src/com/recepcaohotel/controller/ADMNovoQuartoController.java
@@ -1,13 +1,22 @@
 package com.recepcaohotel.controller;
 
 import java.io.IOException;
+import java.util.Optional;
+
+import com.recepcaohotel.app.App;
+import com.recepcaohotel.model.Quarto;
+import com.recepcaohotel.model.Sistema;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
 
 public class ADMNovoQuartoController {
     @FXML
@@ -34,6 +43,33 @@ public class ADMNovoQuartoController {
 
         // OBS.: se houver campos vazios não fazer nada e mostrar uma mensagem para o usuário.
 
+        try {
+            //Verificar os campos antes de usá-los.
+            if (campoNumQuarto.getText().isBlank() || campoQtdCamasCasal.getText().isBlank() ||
+            campoQtdCamasSolteiro.getText().isBlank() || campoValorDiaria.getText().isBlank()){
+
+                mostrarErroVazio();
+                return;
+            }
+
+            int numQuarto = Integer.parseInt(this.campoNumQuarto.getText()); 
+            int qntCamaCasal = Integer.parseInt(this.campoQtdCamasCasal.getText());
+            int qntCamaSolteiro = Integer.parseInt(this.campoQtdCamasSolteiro.getText());
+            float valorDiaria = Float.parseFloat(this.campoValorDiaria.getText());
+
+            Quarto quarto = new Quarto(numQuarto, qntCamaCasal, qntCamaSolteiro, valorDiaria);
+            App.getSystemInstance().adicionarQuarto(quarto);
+        }
+
+        //Erro caso seja digitado algo que não possa ser convertido para números.
+        catch( NumberFormatException exception ){
+            Alert alerta = new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Erro de preenchimento");
+            alerta.setContentText("Por favor, digite apenas números nos espaços e, na diária, digite apenas o valor dividindo os centavos com um ponto (Ex: 548.25)");
+            alerta.showAndWait();
+            return;
+        }
+        
         if (event.getSource() == botaoConfirmar) {
             voltar(botaoConfirmar);
         }
@@ -53,5 +89,13 @@ public class ADMNovoQuartoController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void mostrarErroVazio(){
+
+        Alert alerta = new Alert(AlertType.ERROR);
+        alerta.setTitle("Erro de preenchimento");
+        alerta.setContentText("Por favor, não deixe nenhum campo vazio, todos devem ser preenchidos.");
+        alerta.showAndWait();
     }
 }
